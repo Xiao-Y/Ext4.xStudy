87c0c4aecc014b8c941e8da68416395104e1a383
Xiao-Y <lyongtao123@126.com>
2015-07-27 20:56:35 +0800

    分页，锁定列

--- WebContent/012MVC/app/view/UserList.js
+++ WebContent/012MVC/app/view/UserList.js
@@ -2,8 +2,8 @@ Ext.define('AM.view.UserList', {
 	extend : 'Ext.grid.Panel',
 	alias : 'widget.userList',
 	title : '用户信息列表',// 标题
-	width : 1000,
-	height : 280,
+	width : 500,
+	height : 300,
 	frame : true,// 面板渲染
 	store : 'UsersStore',// 加载数据
 	selType : 'checkboxmodel',// 复选框选择模式
@@ -21,11 +21,13 @@ Ext.define('AM.view.UserList', {
 		width : 50
 	}, {
 		header : 'Name',
-		dataIndex : 'name',
+		dataIndex : 'name'// ,
+		// 列锁定,在单元格编辑模式下不能使用
+		// locked : true
 	}, {
 		header : 'Age',
 		dataIndex : 'age',
-		width : 50,
+		width : 50// ,
 		field : {// 双击编辑
 			xtype : 'textfield',
 			allowBlank : false
