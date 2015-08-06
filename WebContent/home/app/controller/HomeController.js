b641638f3e5f0dda60781dd91e7b52b61c9fa57a
Administrator <Administrator@liuyongtao>
2015-08-06 18:39:34 +0800

    完成菜单的点击

--- WebContent/home/app/controller/HomeController.js
+++ WebContent/home/app/controller/HomeController.js
@@ -3,5 +3,26 @@ Ext.define('AM.controller.HomeController', {
 	// 将Viewport.js添加到控制器
 	views : [ 'Viewport', 'MenuTreeView' ],
 	stores : [ 'MenuTreeStore' ],
-	model : [ 'MenuTreeModel' ]
+	model : [ 'MenuTreeModel' ],
+	// 通过init函数来监听视图事件，控制视图与控制器的交互
+	init : function() {
+		// init函数通过this.control来负责监听
+		this.control({
+			// 被监听的组件的别名
+			'menutree' : {
+				// 监听鼠标点击事件，点击后调用changePage方法
+				itemclick : this.changePage,
+			}
+		});
+	},
+	changePage : function(view, rec, item, index, e) {
+		// 获取url地址
+		var url = rec.get('url');
+		// 获取当前节点信息
+		var title = rec.get('text');
+		// 将主体内容部分的url地址指定为我们获取到的url
+		Ext.getDom('contentIframe').src = url;
+		// 将主体内容框的标题设置为我们获取的节点信息
+		Ext.getCmp('mainContent').setTitle(title);
+	}
 });
